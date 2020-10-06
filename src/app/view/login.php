99f11ce7854cb353ebca16c93fdce96d84592718
Carlos Balbuena <carlos.balbuena@bmtask.com>
2020-10-06 17:36:50 -0500

    mdb files, head, js and login page

--- src/app/view/login.php
+++ src/app/view/login.php
@@ -27,61 +27,111 @@
 <html lang="en">
 	<head>
 		<?php include($html->componentView("head")); ?>
+        <style>
+            .background-header{
+                height: 38vh;
+                width: 100%;
+                position: absolute;
+                top: 0;
+                z-index: 1;
+                background-color: var(--bmt-gray-light);
+            }
+
+            .card{
+                min-width: 450px;
+                margin-top:5em;
+            }
+
+            footer{
+                position: fixed;
+                bottom: 0;
+                width: 100%;
+            }
+
+            @media (max-height: 812px) {
+                footer{
+                    position: relative;
+                }
+            }
+
+            @media (max-width: 1200px) {
+                .card{
+                    margin-top:3em;
+                    min-width: 350px;
+                }
+                .navbar-logo {
+                    max-width: 190px;
+                }
+            }
+
+            @media (max-width: 321px) {
+                .card{
+                    min-width: 300px;
+                    margin-top:3em;
+                }
+            }
+        </style>
 	</head>
-	<body>
-		<div class="navbar navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container-fluid">
-					<div class="branding">
-						<div class="logo">
-							<a href="<?php echo $html->link("index"); ?>"><img src="img/logo_nuevo_s.png" style="height:40px;" alt="Logo"></a>
-						</div>
-					</div>
-					<!--<ul class="nav pull-right">
-						<li><a href="#"><i class="icon-share-alt icon-white"></i> Go to Main Site</a></li>
-					</ul>-->
-				</div>
-			</div>
-		</div>
-		<div class="login-container">
-			
-			<div class="well-login">
-				<?php
-					if(isset($error)){
-				?>
-				<div class="alert alert-error fade in">
-					<button data-dismiss="alert" class="close" type="button">×</button>
-					<strong>Error!</strong> <?php echo $error; ?>
-				</div>
-				<?php
-					}
-				?>
-				<form method="POST" action="" style="margin:0;">
-					<div class="control-group">
-						<div class="controls">
-							<div>
-								<input name="email" type="text" placeholder="Correo electronico" class="login-input user-name">
-							</div>
-						</div>
-					</div>
-					<div class="control-group">
-						<div class="controls">
-							<div>
-								<input name="password" type="password" placeholder="Contraseña" class="login-input user-pass">
-							</div>
-						</div>
-					</div>
-					<input type="hidden" name="send_hidden_login" value="<?php echo $_SESSION["hidden"]["send_hidden_login"]; ?>" />
-					<div class="clearfix">
-						<button class="btn btn-inverse login-btn" type="submit">Ingresar</button>
-					</div>
-					<div class="remember-me">
-						<!--<input class="rem_me" type="checkbox" value=""> Remeber Me-->
-						<a href="<?php echo $html->link("password_reset"); ?>" />Olvido su contraseña?</a>
-					</div>
-				</form>
-			</div>
-		</div>
-		<?php include($html->componentView("js")); ?>
-	</body>
+    <body>
+        <nav class="navbar navbar-light bg-white d-flex flex-column justify-content-center align-items-center" style="height: 15vh; z-index:2; min-height: 140px;">
+            <div class="d-flex navbar-logo">
+                <a href="<?php echo $html->link("index"); ?>"><img width="150px" height="auto" src="img/logoBMT.png" alt="logoBMT"></a>
+            </div>
+        </nav>
+        <div class="d-flex flex-row justify-content-center align-items-start" style="height: 78vh;">
+            <div class="background-header"></div>
+            <div class="container d-flex flex-column justify-content-center align-items-center">
+                <div class="card text-center" style="z-index:2;">
+                    <div class="card-header bg-gray-dark py-3">
+                        <span style="color:white; font-size: 1.5em; font-weight:bold;">Iniciar sesión</span>
+                    </div>
+                    <div class="card-body p-5">
+                        <?php
+                            if(isset($error)){
+                        ?>
+                            <div class="d-flex flex-row justify-content-center alert alert-danger alert-dismissible fade show" role="alert">
+                                <p class="small"><strong>¡Error!</strong> <?php echo $error; ?></p>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        <?php
+                            }
+                        ?>
+                        <form method="POST" action="" style="margin:0;">
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" id="email" class="form-control" />
+                                <label class="form-label" for="email">E-mail</label>
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="password" class="form-control" />
+                                <label class="form-label" for="password">Contraseña</label>
+                            </div>
+
+                            <!-- Submit button -->
+                            <input type="hidden" name="send_hidden_login" value="<?php echo $_SESSION["hidden"]["send_hidden_login"]; ?>" />
+                            <button type="submit" class="btn btn-success btn-block">Iniciar Sesión</button>
+
+                        </form>
+                        <div class="row mt-4">
+                            <div class="col d-flex justify-content-center">
+                                <a href="<?php echo $html->link("password_reset"); ?>" class="link-warning">¿Olvidaste tu contraseña?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="bg-gray text-center text-lg-left">
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: var(--bmt-gray-light);">
+                Copyright © 2020 Business Management Task
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <?php include($html->componentView("js")); ?>
+    </body>
 </html>
